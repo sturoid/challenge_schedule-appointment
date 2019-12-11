34ffd4efd8fcbfdfd3d557a6c68454e784d6d6a2
Stuart Clove <stuart.clove@gmail.com>
2019-12-11 14:15:48 -0700

    Remove unused packages.

--- api/src/app/graphql-resolvers.js
+++ api/src/app/graphql-resolvers.js
@@ -1,4 +1,4 @@
-const merge = require('lodash.merge');
+const { merge } = require('lodash');
 
 const UserResolvers = require('../user/user-resolvers');
 const AppointmentResolvers = require('../appointment/appointment-resolvers');
